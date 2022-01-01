3d940cb2571768299d2fda8859b3ea2ab76597c4
abdelghafour77 <miso.abdo@gmail.com>
2022-01-01 13:58:33 +0100

    v 0.1

--- src/main/java/ac/upm/pharmacy/service/impl/StockServiceImpl.java
+++ src/main/java/ac/upm/pharmacy/service/impl/StockServiceImpl.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public class StockServiceImpl implements StockService {
+public class StockServiceImpl {
     final StockRepository stockRepository;
 
     public StockServiceImpl(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
-    @Override
-    public Stock save(Stock stock) {
-        return stockRepository.save(stock);
-    }
+//    @Override
+//    public Stock save(Stock stock) {
+//        return stockRepository.save(stock);
+//    }
 }
